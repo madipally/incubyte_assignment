5a64f8ff63cd7d7c1490e204567277ab7f5fc634
Madipally Naveen Kumar <madipallynaveen@gmail.com>
2024-07-09 18:07:54 +0530

    Optimizing the code by adding condition

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,16 +6,18 @@ class StringCalculator
         custom_delimiter_section, input = input.split("\n")
         custom_delimiters = custom_delimiter_section[2..-1]
         delimiters += custom_delimiters.scan(/\[(.*?)\]/).flatten
-        delimiters << custom_delimiters
+        delimiters << custom_delimiters unless custom_delimiters.include?("[")
       end
 
       numbers = input.split(Regexp.union(delimiters)).map(&:to_i)
 
       negatives = numbers.select{|num| num < 0}
       raise "Negatives not allowed: #{negatives.join(',')}" if negatives.any?
-      
+
       numbers.sum
     end
 end
 
 # StringCalculator.new.add("//;\n1;2")
+StringCalculator.new.add("//[*][%]\n1*2%3")
+StringCalculator.new.add("//[$$$]\n1$$$2$$$3")
