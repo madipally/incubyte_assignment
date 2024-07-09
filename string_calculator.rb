b218f3270a098499c8576efee2dab73e72294ec7
Madipally Naveen Kumar <madipallynaveen@gmail.com>
2024-07-09 16:55:37 +0530

    Adding comma-separated numbers

---
+++ string_calculator.rb
@@ -0,0 +1,6 @@
+class StringCalculator
+    def add(input)
+      return 0 if input.empty?
+      input.split(',').map(&:to_i).sum
+    end
+end
---
+++ string_calculator_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+    let(:calculator) { StringCalculator.new }
+    
+    describe '#add' do
+      context 'with numbers separated by commas' do
+        it 'returns the sum of the numbers' do
+          expect(calculator.add("1,2,3")).to eq(6)
+        end
+      end
+    end
+  end
